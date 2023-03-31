3a6f001452ddf1d72648c5115532be73c7ebbd66
abhinavyadav <yadavabhinavkumar@gmail.com>
2023-04-01 05:20:47 +0530

    Add

--- src/mainRoutes.routes.ts
+++ src/mainRoutes.routes.ts
@@ -1,5 +1,6 @@
 import express, { Router } from "express";
 import { AuthRoutes } from "./auth/auth.routes";
+import { ClassroomRoutes } from "./classroomModule/classroom/classroom.routes";
 import { CommentRoutes } from "./communityModule/comments/comment.routes";
 import { LikeRoutes } from "./communityModule/likes/like.routes";
 import { PostRoutes } from "./communityModule/post.routes";
@@ -11,5 +12,7 @@ app.use("/auth", AuthRoutes);
 app.use("/post", PostRoutes);
 app.use("/comment", CommentRoutes);
 app.use("/likes", LikeRoutes);
+app.use("/classroom", ClassroomRoutes);
+
 
 module.exports = app;
